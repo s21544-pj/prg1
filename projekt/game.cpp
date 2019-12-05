73cf9a84e69c6a3526b5b136ba4eb8544cc133b0
s21544 <s21544@pjwstk.edu.pl>
2019-12-05 11:11:59 +0100

    projekt - gra paragrafowa

---
+++ projekt/game.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Game {
+private:
+    vector<Room> Rooms;
+};
+class Room {
+private:
+    string Layout;
+}
---
+++ projekt/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include Game.cpp
+
+using namespace std;
+
+int main (){
+
+}
